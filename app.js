b2e24535f957da7a868a2ce32e2a28d15e30147c
yisoo song <yisoo1049@gmail.com>
2024-05-25 08:22:19 +0900

    last

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const indexRouter = require('./routes/index')
+require('dotenv').config()
+const cors = require('cors')
 const app = express()
+const MONGODB_URI_PROD= process.env.MONGODB_URI_PROD
+console.log('monmon',MONGODB_URI_PROD)
 app.use(bodyParser.json())
+app.use(cors())
 app.use('/api',indexRouter)
-const mongoURI = `mongodb://localhost:27017/todo-demo`
+const mongoURI = MONGODB_URI_PROD
 
 //useNewUrlParser = 몽고db가 주소가 여러가지 있음, 옛날거 요즘꺼 다 잘 쓰게해달라는 뜻
 mongoose.connect(mongoURI,{useNewUrlParser:true})
@@ -16,6 +21,6 @@ mongoose.connect(mongoURI,{useNewUrlParser:true})
         console.log('DB connection fail', err);
     });
 
-app.listen(5000,()=>{
+app.listen(process.env.PORT || 5000,()=>{
     console.log('server on 5000');
 });
